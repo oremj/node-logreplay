af41ac7e8d89d355ffc0e1c8488e89d8903dde6f
Jeremiah Orem <oremj@oremj.com>
2012-10-06 01:16:51 -0700

    clean up load test class a bit.

--- main.js
+++ main.js
@@ -12,19 +12,25 @@ var pool_size = argv.c;
 var requests = argv.n;
 https.globalAgent.maxSockets = pool_size;
 
-function doRequest(callback) {
+function LoadTest(requests, pool_size) {
     var self = this;
+    self.requests = requests;
+    self.pool_size = pool_size;
+    self.running = 0;
+}
+
+LoadTest.prototype.doRequest = function(callBack) {
     var start;
     var stats = {
         'res_time': null,
         'status': null
     }
     var req = https.request({host: 'addons-dev.allizom.org',
-                             path: '/media/updater.output.txt'}, function(res) {
+                             path: '/en-US/firefox/'}, function(res) {
                     res.on('end', function() {
                         stats.res_time = Date.now() - start;
                         stats.status = res.statusCode;
-                        callback(stats);
+                        callBack(stats);
                     });
               });
 
@@ -34,18 +40,38 @@ function doRequest(callback) {
     req.end();
 }
 
-function next() {
-    requests--;
-    if(requests < 0) {
+LoadTest.prototype.end = function() {
+    elapsed_s = (Date.now() - this.start) / 1000;
+    console.info("%d Reqs/sec", this.good_requests / elapsed_s);
+}
+
+LoadTest.prototype.run = function() {
+    this.start = Date.now();
+    this.good_requests = 0;
+
+    for(i=0; i < this.pool_size; i++) {
+        this.next();
+    }
+}
+
+LoadTest.prototype.next = function() {
+    var self = this;
+    if(self.requests <= 0) {
+        if(self.running == 0) {
+            self.end();
+        }
         return;
     }
-    doRequest(function(stats) {
+
+    self.requests--;
+    self.running++;
+    self.doRequest(function(stats) {
         console.info("Request took: %dms and returned %s", stats.res_time, stats.status);
-        next();
+        self.good_requests++;
+        self.running--;
+        self.next();
     });
 }
 
-
-for(i=0; i< pool_size; i++) {
-    next();
-}
+l = new LoadTest(requests, pool_size);
+l.run();
